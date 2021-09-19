a9037b30c0084769b0b3f532518d004dbd6c4f4a
Serzhje <7prite@gmail.com>
2021-09-19 09:51:54 +0300

    5.7. Ипотека [#156315]

---
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,14 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+    public static int year(double amount, double salary, double percent) {
+        int year = 0;
+        percent /= 100;
+        while (amount > 0) {
+            amount += amount * percent;
+            amount -= salary;
+            year++;
+        }
+        return year;
+    }
+}
